4a338a258161c2d51e3b82f10379b15f1ec6d2cf
Bishesh99 <bisheshsingh.99@gmail.com>
2023-11-22 21:00:05 +0530

    Removing Sercurity Config

--- src/main/java/com/remote/config/remoteconfigproviderservice/RemoteConfigProviderServiceApplication.java
+++ src/main/java/com/remote/config/remoteconfigproviderservice/RemoteConfigProviderServiceApplication.java
@@ -1,15 +1,39 @@
 package com.remote.config.remoteconfigproviderservice;
 
 import com.flagsmith.FlagsmithClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableWebSecurity
 public class RemoteConfigProviderServiceApplication {
+    @Bean
+    public UserDetailsService userDetailsService(@Autowired final PasswordEncoder passwordEncoder,
+                                                 @Value("${USER_ID}") final String userId,
+                                                 @Value("${USER_PASS}") final String password) {
+        final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User
+                .withUsername(userId)
+                .password(passwordEncoder.encode(password)).build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public FlagsmithClient getFlagsmithClient(@Value("${API_TOKEN}") final String apiKey) {
         return FlagsmithClient.newBuilder()
