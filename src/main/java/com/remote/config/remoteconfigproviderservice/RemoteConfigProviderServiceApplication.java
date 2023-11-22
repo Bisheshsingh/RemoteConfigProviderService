11c1454bf0a0eb082b9bd60d8108f07711786b10
Bishesh99 <bisheshsingh.99@gmail.com>
2023-11-22 12:52:03 +0530

    Using api key of sercrets in git instead of environment variable

--- src/main/java/com/remote/config/remoteconfigproviderservice/RemoteConfigProviderServiceApplication.java
+++ src/main/java/com/remote/config/remoteconfigproviderservice/RemoteConfigProviderServiceApplication.java
@@ -1,12 +1,22 @@
 package com.remote.config.remoteconfigproviderservice;
 
+import com.flagsmith.FlagsmithClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
 public class RemoteConfigProviderServiceApplication {
+    @Bean
+    public FlagsmithClient getFlagsmithClient(@Value("${api.token}") final String apiKey) {
+        return FlagsmithClient.newBuilder()
+                .setApiKey(apiKey)
+                .build();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RemoteConfigProviderServiceApplication.class, args);
     }
--- src/main/java/com/remote/config/remoteconfigproviderservice/ServiceConfigRepository.java
+++ src/main/java/com/remote/config/remoteconfigproviderservice/ServiceConfigRepository.java
@@ -1,12 +1,10 @@
 package com.remote.config.remoteconfigproviderservice;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flagsmith.FlagsmithClient;
-import com.flagsmith.exceptions.FlagsmithClientError;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.config.server.environment.EnvironmentRepository;
@@ -16,10 +14,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ServiceConfigRepository implements EnvironmentRepository {
-    @Value("${API_TOKEN}")
-    private String apiKey;
-    @Autowired
+    private FlagsmithClient flagsmithClient;
     private ObjectMapper objectMapper;
 
     @Override
@@ -27,29 +24,20 @@ public class ServiceConfigRepository implements EnvironmentRepository {
         Map<String, String> properties;
 
         try {
-            properties = loadCustomProperties(application);
+            properties = objectMapper
+                    .readValue(flagsmithClient.getEnvironmentFlags()
+                            .getFeatureValue(application).toString(),
+                            new TypeReference<>() {});
         } catch (Exception e) {
             properties = new HashMap<>();
         }
 
-        final PropertySource propertySource = new PropertySource("custom", properties);
-
-        final Environment environment = new Environment(application, profile,
-                label, "master");
+        final PropertySource propertySource = new PropertySource(label, properties);
+        final Environment environment = new Environment(application,
+                profile, label, "master");
 
         environment.add(propertySource);
 
         return environment;
-    }
-
-    private Map<String, String> loadCustomProperties(final String application) throws FlagsmithClientError, JsonProcessingException {
-        final FlagsmithClient flagsmithClient = FlagsmithClient.newBuilder()
-                .setApiKey(apiKey)
-                .build();
-
-        return objectMapper
-                .readValue(flagsmithClient.getEnvironmentFlags()
-                        .getFeatureValue(application).toString(), new TypeReference<>() {
-                });
     }
 }
